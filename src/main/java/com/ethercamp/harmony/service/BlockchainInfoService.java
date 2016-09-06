2bb66465356a988fa252ffba41f03c33148fa993
Stan Reshetnyk <stan@ether.camp>
2016-09-06 09:08:01 +0300

    Add Ether Camp license over java files

--- src/main/java/com/ethercamp/harmony/service/BlockchainInfoService.java
+++ src/main/java/com/ethercamp/harmony/service/BlockchainInfoService.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2015, 2016 Ether.Camp Inc. (US)
+ * This file is part of Ethereum Harmony.
+ *
+ * Ethereum Harmony is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ethereum Harmony is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ethereum Harmony.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.ethercamp.harmony.service;
 
 import ch.qos.logback.classic.Level;
@@ -135,7 +153,6 @@ public class BlockchainInfoService implements ApplicationListener {
 
         LongStream.rangeClosed(startImportBlock, lastBlock)
                 .forEach(i -> {
-                    log.debug("Imported block " + i);
                     addBlock(blockchain.getBlockByNumber(i));
                 });
     }
