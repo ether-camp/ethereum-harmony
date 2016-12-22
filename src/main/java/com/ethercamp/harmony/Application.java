a2ecfef1dd779a63f84a48efeff27c0cb8c9ad75
Stan Reshetnyk <stan@ether.camp>
2016-12-22 15:34:54 +0200

    Add command line way to import blocks

--- src/main/java/com/ethercamp/harmony/Application.java
+++ src/main/java/com/ethercamp/harmony/Application.java
@@ -19,17 +19,36 @@
 package com.ethercamp.harmony;
 
 import com.ethercamp.harmony.config.EthereumHarmonyConfig;
+import org.ethereum.Start;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 @SpringBootApplication
 @EnableScheduling
 @Import({EthereumHarmonyConfig.class})
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(new Object[]{Application.class}, args);
+    /**
+     * Does one of:
+     * - start Harmony peer;
+     * - perform action and exit on completion.
+     */
+    public static void main(String[] args) throws Exception {
+        final List<String> actions = asList("importBlocks");
+
+        System.getProperties().entrySet()
+                .forEach(e -> System.out.println("System prop " + e.getKey() + "=" + e.getValue()));
+
+        if (asList(args).stream().anyMatch(arg -> actions.contains(arg))) {
+            Start.main(args);
+        } else {
+            SpringApplication.run(new Object[]{Application.class}, args);
+        }
     }
 }
