3c4a4d9a2a6d71296873c8d6c6c1b04e5d429368
Duncan Brown <duncan@bhs-consultants.com>
2017-03-29 21:35:54 -0400

    Allows for Web3j calls to successfully return errors in a format that Web3j expects. See the error resolver in this commit for details. Also added a JVM config option that will enable this feature. The feature is disabled by default.

--- src/main/java/com/ethercamp/harmony/config/ApplicationConfig.java
+++ src/main/java/com/ethercamp/harmony/config/ApplicationConfig.java
@@ -29,6 +29,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.ethercamp.harmony.util.exception.Web3jSafeAnnotationsErrorResolver;
+
 /**
  * Created by Stan Reshetnyk on 18.07.16.
  */
@@ -36,15 +38,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ComponentScan("com.ethercamp")
 public class ApplicationConfig extends WebMvcConfigurerAdapter {
 
+    private final String errorResolverKey = "errorResolver.web3jCompliant";
+
     /**
      * Export bean which will find our json-rpc bean with @JsonRpcService and publish it.
      * https://github.com/briandilley/jsonrpc4j/issues/69
      */
     @Bean
-    @SuppressWarnings("deprecation")
+    @SuppressWarnings({"unchecked", "deprecation"})
     // full class path to avoid deprecation warning
     public com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceExporter exporter() {
-        return new com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceExporter();
+        com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceExporter serviceExporter = new com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceExporter();
+
+        if (System.getProperty(this.errorResolverKey) != null && 
+            System.getProperty(this.errorResolverKey).toUpperCase().equals("TRUE")) {
+          serviceExporter.setErrorResolver(Web3jSafeAnnotationsErrorResolver.INSTANCE);
+        }
+
+        return serviceExporter;
     }
 
     /**
